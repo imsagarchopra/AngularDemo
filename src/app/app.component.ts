71ab2f9757c2892384115377d600a7152dff3ec0
Sagar Chopra <schopra@firemate.com>
2022-12-28 17:37:53 +0530

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageHeader: string = 'Employee Details';
+  firstName: string = 'Tom';
+  lastName: string = 'Hopkins';
+  imagePath: string = "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png";
+
+  getFullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
 }
