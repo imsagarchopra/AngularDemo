f84c08f660f7109db9d13065a83b4c3f181fed99
Sagar Chopra <schopra@firemate.com>
2022-12-28 16:22:24 +0530

    Angular Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello {{name}}</h1>`
 })
 export class AppComponent {
-  title = 'AngularDemo';
+  name: string = 'Angular';
 }
