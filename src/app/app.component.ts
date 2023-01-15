96ba3f74db888c38a871f1daaaa6c296a26d2308
Sagar Chopra <schopra@firemate.com>
2023-01-15 10:11:28 +0530

    Style Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<button class="colorClass" [class] ='classesToApply' > My Button </button>
-              <br/><br/>
-              <button class="colorClass" [class.boldClass] ='!applyBoldClass' > My Button </button>
-              <br/><br/>
-              <button class="colorClass" [ngClass] ='addClasses()' > My Button </button>
-            `
+  //template: `<button class="colorClass" [class] ='classesToApply' > My Button </button>
+              //<br/><br/>
+              //<button class="colorClass"d [class.boldClass] ='!applyBoldClass' > My Button </button>
+              //<br/><br/>
+              //<button class="colorClass" [ngClass] ='addClasses()' > My Button </button>
+          //  `
   //templateUrl: 'app.component.html'
+  template: `<button style = 'color: red' [style.font-weight] = "isBold ? 'bold' : 'normal'">My Button</button>
+             <br/><br/>
+              <button style = 'color: red' [style.font-size.px] = "fontSize">My Button</button>
+              <br/><br/>
+              <button style = 'color: red' [ngStyle] = "addStyles()">My Button</button>
+            `
+             
 })
 export class AppComponent {
   pageHeader: string = 'Employee Details';
@@ -30,5 +37,18 @@ export class AppComponent {
       italicsClass: this.applyItalicsClass
     };
     return classes;
+  }
+
+  isBold: boolean = true;
+  fontSize: number = 30;
+  isItalics: boolean = true;
+
+  addStyles() {
+    let styles = {
+      'font-size.px': this.fontSize,
+      'font-style': this.isItalics ? 'italic' : 'normal',
+      'font-weight': this.isBold ? 'bold' : 'normal',
+    };
+    return styles;
   }
 }
