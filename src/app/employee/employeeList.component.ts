4fc0a2dafb04993f4fee4314e95e6bb0f89f70e9
Sagar Chopra <schopra@firemate.com>
2023-01-15 12:46:23 +0530

    track by to improve ngFor performance ngFor Directives: index first last even odd

--- src/app/employee/employeeList.component.ts
+++ src/app/employee/employeeList.component.ts
@@ -6,10 +6,43 @@ import { Component } from '@angular/core';
   styleUrls:['employeeList.component.css']
   })
 export class EmployeeListComponent {
-  employees: any[] = [
-    { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/6/1988' },
-    { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
-    { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
-    { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '14/10/1980' },
-  ];
+  employees: any[];
+
+  constructor() {
+    this.employees = [
+      { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/6/1988' },
+      { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
+      { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
+      { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '14/10/1980' },
+    ];
+  }
+
+  getEmployees(): void {
+    this.employees = [
+      {
+        code: 'emp101', name: 'Tom', gender: 'Male',
+        annualSalary: 5500, dateOfBirth: '25/6/1988'
+      },
+      {
+        code: 'emp102', name: 'Alex', gender: 'Male',
+        annualSalary: 5700.95, dateOfBirth: '9/6/1982'
+      },
+      {
+        code: 'emp103', name: 'Mike', gender: 'Male',
+        annualSalary: 5900, dateOfBirth: '12/8/1979'
+      },
+      {
+        code: 'emp104', name: 'Mary', gender: 'Female',
+        annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+      },
+      {
+        code: 'emp105', name: 'Nancy', gender: 'Female',
+        annualSalary: 6700.826, dateOfBirth: '15/12/1982'
+      },
+    ];
+  }
+
+  trackByEmpCode(index: number, employee: any) {
+    return employee.code;
+  }
 }
