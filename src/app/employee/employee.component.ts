088dfe4a4970f3cc01a687e6e1da0692742bdeeb
Sagar Chopra <schopra@firemate.com>
2023-01-24 18:22:19 +0530

    Angular router navigate method

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IEmployee } from './employee';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { EmployeeService } from './employee.service';
 
 @Component({
@@ -24,8 +24,12 @@ export class EmployeeComponent implements OnInit {
   employee!: IEmployee;
   statusMessage: string = 'Loading data. Please wait...';
 
-  constructor(private _employeeService: EmployeeService, private _activatedRoute: ActivatedRoute) {
+  constructor(private _employeeService: EmployeeService, private _activatedRoute: ActivatedRoute, private _router: Router) {
 
+  }
+
+  onBackButtonClick(): void {
+    this._router.navigate(['/employees']);
   }
 
   ngOnInit() {
